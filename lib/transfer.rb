b31cbe4c1c3abe1a53239e35b8ee26101ad85887
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2020-02-03 18:09:47 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,18 +1,19 @@
 require 'pry'
 class Transfer
-  attr_accessor :sender, :receiver, :amount, :status 
+  attr_accessor :sender, :receiver, :amount, :status
+
   def initialize(sender, receiver, amount)
-    @sender = sender 
+    @sender = sender
     @receiver = receiver
     @amount = amount
     @status = "pending"
-    
-end
+  end
 
-def valid? 
-  @sender.valid? && @receiver.valid? ? true : false 
-end 
-def execute_transaction
+  def valid?
+    @sender.valid? && @receiver.valid? ? true : false
+  end
+
+  def execute_transaction
     if @sender.balance > @amount && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
